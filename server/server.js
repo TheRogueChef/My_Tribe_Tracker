278339c029bc5fc38d77d28616fc35c581fa955a
Erik Carlson <theroguechef@comcast.net>
2023-09-04 16:57:28 -0700

    lotsa clean up

--- server/server.js
+++ server/server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const multer = require('multer');
 require('./config/mongoose.config');
-const cookieParser = require('cookie-parser');
+
 
 require('dotenv').config();
 
-
-const upload = multer({ dest: 'uploads/' });
-const Pic = require('./models/picModel');
 
 const app = express();
 const port = 8000;
@@ -18,10 +14,10 @@ const port = 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('uploads/'));
+
 
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
-app.use(cookieParser());
+
 
 const userRoutes = require('./routes/userRoutes');
 const diaryRoute = require('./routes/diaryRoute');
@@ -29,7 +25,6 @@ const eventRoute = require('./routes/eventRoute');
 const familyRoute = require('./routes/familyRoute');
 const songRoute = require('./routes/songRoute');
 const likeRoute = require('./routes/likeRoute');
-const picRoute = require('./routes/picRoute');
 const chatRoute = require('./routes/chatRoute');
 
 
@@ -41,36 +36,7 @@ familyRoute(app);
 songRoute(app);
 likeRoute(app);
 chatRoute(app);
-app.use(picRoute);
 
-app.post('/api/newPic', upload.single('image'), (req, res) => {
-    console.log('Received data from the client:', req.body);
-    const { picTitle, picDate, picLocation } = req.body;
-
-    console.log('Made it past the upload:', req.body);
-    if (req.file) {
-        const image = req.file.filename;
-
-        const newPic = new Pic({
-            picTitle,
-            picDate,
-            picLocation,
-            image,
-        });
-
-
-        newPic.save()
-            .then(() => {
-                res.status(200).json({ message: 'Pic created successfully' });
-            })
-            .catch(() => {
-                res.status(500).json({ message: 'Failed to create the pic' });
-            });
-    } else {
-        res.status(400).json({ message: 'No image file provided' });
-    }
-
-});
 
 
 
